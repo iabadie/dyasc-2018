c4cd0b99fd5be5048d9caf373b9434e85fb99442
Nacho <ignacioabadie@live.com.ar>
2018-08-20 15:09:27 -0300

    codigo final y mvn buildeado

--- fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -1,11 +1,25 @@
 package ar.edu.untref.dyasc;
 
 public class Program {
+    static int f0 = 0;
+    static int f1 = 1;
 
     public static void main(String[] args) {
-    
-        System.out.println("Hola a todos!!!");
-    
+        int input = Integer.parseInt(args[0]);
+        String result = "Fibo<" + args[0] + ">:";
+        for (int x = 0; x < input ; x++){
+            result += " " + String.valueOf(getValue(x));
+        }
+        System.out.println(result);
     }
-
+    
+    private static int getValue(int x) {
+        if (x == 0) {
+            return f0;
+        }
+        if (x == 1) {
+            return f1;
+        }
+        return getValue(x-1) + getValue(x-2);
+    }
 }
