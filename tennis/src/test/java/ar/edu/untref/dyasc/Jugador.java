6dd3686a7bb7b5e6480624aab0465093b1b7718a
Nacho <ignacioabadie@live.com.ar>
2018-10-05 19:18:46 -0300

    test un jugador suma un punto

---
+++ tennis/src/test/java/ar/edu/untref/dyasc/Jugador.java
@@ -0,0 +1,10 @@
+package ar.edu.untref.dyasc;
+
+public class Jugador {
+
+    public void anotarPunto() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ tennis/src/test/java/ar/edu/untref/dyasc/TestTenis.java
@@ -0,0 +1,12 @@
+package ar.edu.untref.dyasc;
+
+import org.junit.Test;
+
+public class TestTenis {
+    
+    @Test
+    public void unJugadorSumaUnPunto() {
+        Jugador jugador1 = new Jugador();
+        jugador1.anotarPunto();
+    }
+}
