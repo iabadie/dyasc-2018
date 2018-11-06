b119cecd76e709b85bf5afbf93856d093b34c0b6
Nacho <ignacioabadie@live.com.ar>
2018-11-06 09:07:46 -0300

    AgregarUnBoteOcupaUnaCasilla

--- batalla_naval/src/test/java/ar/edu/untref/dyasc/TestBatallaNaval.java
+++ batalla_naval/src/test/java/ar/edu/untref/dyasc/TestBatallaNaval.java
@@ -2,11 +2,14 @@ package ar.edu.untref.dyasc;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class TestBatallaNaval {
     @Test
     public void agregarUnBoteOcupaUnaCasilla(){
         Bote bote = new Bote();
         Casilla casilla = new Casilla();
-        
+        casilla.agregarBote(bote);
+        Assert.assertNotNull(casilla.revisarCasilla());
     }
 }
