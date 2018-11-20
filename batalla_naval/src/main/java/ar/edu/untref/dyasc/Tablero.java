98b730299988007f5b68ce78445b886304a635f2
Nacho <ignacioabadie@live.com.ar>
2018-11-19 21:20:37 -0300

    test agregar un bote en un tablero

--- batalla_naval/src/main/java/ar/edu/untref/dyasc/Tablero.java
+++ batalla_naval/src/main/java/ar/edu/untref/dyasc/Tablero.java
@@ -1,17 +1,25 @@
 package ar.edu.untref.dyasc;
 
 public class Tablero {
-    Casilla[][] tablero;
+    Bote[][] tablero;
     int x;
     int y;
 
     public Tablero(int x, int y) {
-        this.tablero = new Casilla[x][y];
+        this.tablero = new Bote[x][y];
         this.x = x;
         this.y = y;
     }
     
     public String obtenerTamanoDeTablero() {
         return x + "x" + y;
+    }
+
+    public void agregarBoteEnCasilla(Bote bote, int x, int y) {
+        tablero[x-1][y-1] = bote;
+    }
+
+    public Bote obtenerCasilla(int x, int y) {
+        return tablero[x-1][y-1];
     }
 }
--- batalla_naval/src/test/java/ar/edu/untref/dyasc/TestTablero.java
+++ batalla_naval/src/test/java/ar/edu/untref/dyasc/TestTablero.java
@@ -11,4 +11,12 @@ public class TestTablero {
         Tablero tablero = new Tablero(4,2);
         Assert.assertEquals("4x2", tablero.obtenerTamanoDeTablero());
     }
+    
+    @Test
+    public void testAgregarUnBoteEnUnaCasilla() {
+        Tablero tablero = new Tablero(4,2);
+        Bote bote = new Bote();
+        tablero.agregarBoteEnCasilla(bote,3,1);
+        Assert.assertEquals(bote, tablero.obtenerCasilla(3,1));
+    }
 }
